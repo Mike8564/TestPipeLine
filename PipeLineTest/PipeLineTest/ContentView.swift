c1003790692727489c46dd904b8eda332d201800
Mike <54718913+Mike8564@users.noreply.github.com>
2024-03-11 13:31:41 -0400

    Add files via upload

---
+++ PipeLineTest/PipeLineTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PipeLineTest
+//
+//  Created by Mike Sedaghatnia on 2024-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PipeLineTest/PipeLineTest/PipeLineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipeLineTestApp.swift
+//  PipeLineTest
+//
+//  Created by Mike Sedaghatnia on 2024-03-11.
+//
+
+import SwiftUI
+
+@main
+struct PipeLineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
